a9909113208b3dd48d1adc50cac0d3dd18cbd7b8
cmptrsn2 <cmptrsn2@illinois.edu>
2025-08-07 20:53:01 -0500

    energy by source figure

---
+++ Basic_Plotting.R
@@ -0,0 +1,30 @@
+path_to_data = "C:/Users/Chels/Documents/Climate/Community-Solar-Repo"
+setwd(path_to_data)
+
+library(readxl)
+library(ggplot2)
+library(reshape)
+
+# read in primary data spreadsheet
+df = read_excel("Community_Solar_Calculations.xlsx", sheet="Savings estimate")
+df = df[-which(is.na(df$Year)),]
+
+# subset to 2024
+df.2024 = df[which(df$Year == 2024),]
+
+# month order
+months = df.2024$Month
+
+# plot energy use by source
+df.simple.2024 = df.2024[,c(2,7,8)]
+colnames(df.simple.2024) = c("Month","Community solar","Utility")
+df.source.melt = melt(df.simple.2024,id.vars=c("Month"))
+ggplot(df.source.melt, aes(x=factor(Month, levels=months), 
+                           y=value, 
+                           color=variable,
+                           group=variable)) + 
+       geom_point() + geom_line() + 
+       scale_color_manual(values=c("Community solar"="darkgoldenrod1",
+                                   "Utility"="darkslategray")) +
+       labs(x="",y="Energy use (kWh)",color="Energy source",
+            title="Energy by source in 2024")
